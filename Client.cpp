70821796356aef468b7b2fe4dd47ef8a043ba421
Protopopova <Alina.Protopopova@billing.ru>
2018-09-22 16:19:40 +1000

    implemented readHGT function

--- Client.cpp
+++ Client.cpp
@@ -4,10 +4,15 @@
 int main()
 {
 	std::size_t fWidth;
-	//int16_t* heights = ;
-	readHGT("N40E127.hgt", &fWidth);
+	int16_t* heights = readHGT("N40E127.hgt", &fWidth);
+	
+	if (heights == NULL)
+		return 0;
 
-  //	delete[] heights;
+	for (int i = 0; i < 5; i++)
+		std::cout << heights[i] << " ";
+
+  	delete[] heights;
  	system("pause");
 	return 0;
 }
--- HGTVisualizer.h
+++ HGTVisualizer.h
@@ -2,75 +2,35 @@
 #ifndef HGT_VISUALIZER
 #define HGT_VISUALIZER
 
-void readHGT(const char* name, std::size_t* fWidth)
+int16_t BigEndianToLittleEndian(uint16_t s)
+{ 
+	return  (s << 8) | (s  >> 8);
+}
+int16_t* readHGT(const char* name, std::size_t* fWidth)
 {
-	//FILE* ptrFile = fopen(name, "rb");
-
-	//if (ptrFile == NULL)
-	//	return 0;
-
-	//// определяем размер файла
-	//fseek(ptrFile, 0, SEEK_END);                         
-	//long lSize = ftell(ptrFile);                            
-	//rewind(ptrFile);                                       
-
-
-	//std::int16_t* heights = (std::int16_t *)malloc(lSize / 2 * sizeof(std::int16_t));
-	////double* heights = new double[lSize / 2];
-	//
-	//*fWidth = std::sqrt(lSize / 2);
-	//size_t result;
-	//for (int i = 0; i < *fWidth; ++i)
-	//	heights[i] = 0;
-
-	//result = fread(&heights[0], 1 * sizeof(std::int16_t), 1, ptrFile);
-
-	//	
-
-	//	std::cout << heights[0];
-
-	//fclose(ptrFile);
-	//return heights;
-
-	int counter;
-	int32_t *ptr_d;
-	FILE *ptr_fp;
+	int16_t* buffer;
+	FILE *hgtFile;
 	
-	if ((ptr_fp = fopen(name, "rb")) == NULL)
-	{
-		printf("Unable to open the file!\n");
-		exit(1);
-	}
-	else printf("Opened file successfully for reading.\n");
+ 	if ((hgtFile = fopen(name, "rb")) == NULL)
+		return NULL;
 
-	fseek(ptr_fp, 0, SEEK_END);
-	long lSize = ftell(ptr_fp);
-	rewind(ptr_fp);
+	fseek(hgtFile, 0, SEEK_END);
+	long lSize = ftell(hgtFile);
+	rewind(hgtFile);
 
-	ptr_d = new int32_t[lSize / 2];
-	std::size_t res;
+	buffer = new int16_t[lSize / 2];
+	*fWidth = lSize / 2;
+
 	for (int i = 0; i < lSize / 2; ++i)
 	{
-		res = fread(&ptr_d[i], 2, 1, ptr_fp);
+		size_t res = fread(&buffer[i], 2, 1, hgtFile);
 		if (!res)
-			exit(1);
+			return NULL;
+		buffer[i] = BigEndianToLittleEndian((uint16_t)buffer[i]);
 	}
-	/*if (fread(ptr_d, lSize, 1, ptr_fp) != 1)
-	{
-		printf("Read error!\n");
-		exit(1);
-	}*/
-	//else printf("Read was successful.\n");
-	fclose(ptr_fp);
 
-	/* Part H */
-	printf("The numbers read from file are:\n");
-	for (counter = 0; counter < 10; counter++)
-		printf("%f ", ptr_d[counter]);
-
-	/* Part I */
-	//free(ptr_d);
-	
+	fclose(hgtFile);
+	return buffer;
 }
 
 #endif
